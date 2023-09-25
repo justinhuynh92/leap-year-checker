3784ae82c60a19aeb70340c780129f6f1547075b
Justin Huynh <justinhuynh639@gmail.com>
2023-09-25 16:33:12 -0700

    finished leap year checker

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 
 #create function for leap year
 def is_leap_year(year):
-    #check if the year is evenly divisible by 4
+    #check if the year is evenly divisible by 4 and 100
     if year % 4 == 0:
-        pass
+        if year % 100 == 0:
+            if year % 400 == 0:
+                print('Leap Year')
+            else:
+                print('Not leap year')
+        else:
+            print('Leap year')
+    else:
+        print('Not leap year')
+
+#call function and check if the input is a leap year
+is_leap_year(2004)
